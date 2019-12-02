9248fe44f4bc3bf680daf1b14581c8b3067d0b89
ljj <1822078666@qq.com>
2019-12-02 22:10:22 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,13 @@
+export default {
+    singular:true,
+    plugins: [
+        ['umi-plugin-react',{
+            //后面按需求配置
+        }]
+    ],
+    routes:[{
+        path:'/',
+        component:'./HelloWorld',
+    }],
+    
+};
